d934baa70b0905a8b197972aee4f418deb16045b
Willhong <hbilly2002@gmail.com>
2022-12-21 05:11:25 +0900

    DataGridView Parse

--- MaplestoryAPI/Form1.Designer.cs
+++ MaplestoryAPI/Form1.Designer.cs
@@ -31,25 +31,29 @@
             this.dtpFrom = new System.Windows.Forms.DateTimePicker();
             this.dtpTo = new System.Windows.Forms.DateTimePicker();
             this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnReadKey = new System.Windows.Forms.Button();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dtpFrom
             // 
-            this.dtpFrom.Location = new System.Drawing.Point(12, 12);
+            this.dtpFrom.Location = new System.Drawing.Point(12, 11);
             this.dtpFrom.Name = "dtpFrom";
             this.dtpFrom.Size = new System.Drawing.Size(200, 21);
             this.dtpFrom.TabIndex = 0;
             // 
             // dtpTo
             // 
-            this.dtpTo.Location = new System.Drawing.Point(218, 12);
+            this.dtpTo.Location = new System.Drawing.Point(218, 11);
             this.dtpTo.Name = "dtpTo";
             this.dtpTo.Size = new System.Drawing.Size(200, 21);
             this.dtpTo.TabIndex = 0;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(424, 22);
+            this.btnSearch.Location = new System.Drawing.Point(424, 10);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(75, 23);
             this.btnSearch.TabIndex = 1;
@@ -57,16 +61,46 @@
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1285, 385);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnReadKey
+            // 
+            this.btnReadKey.Location = new System.Drawing.Point(505, 9);
+            this.btnReadKey.Name = "btnReadKey";
+            this.btnReadKey.Size = new System.Drawing.Size(101, 23);
+            this.btnReadKey.TabIndex = 1;
+            this.btnReadKey.Text = "ReadKeyFile";
+            this.btnReadKey.UseVisualStyleBackColor = true;
+            this.btnReadKey.Click += new System.EventHandler(this.btnReadKey_Click);
+            // 
+            // pBar
+            // 
+            this.pBar.Location = new System.Drawing.Point(12, 440);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(768, 56);
+            this.pBar.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1405, 554);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnReadKey);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.dtpTo);
             this.Controls.Add(this.dtpFrom);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -76,6 +110,9 @@
         private System.Windows.Forms.DateTimePicker dtpFrom;
         private System.Windows.Forms.DateTimePicker dtpTo;
         private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnReadKey;
+        private System.Windows.Forms.ProgressBar pBar;
     }
 }
 
--- MaplestoryAPI/Form1.cs
+++ MaplestoryAPI/Form1.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -52,22 +53,31 @@ namespace MaplestoryAPI
         {
             InitializeComponent();
         }
-        CubeDetail cd2 = new CubeDetail();
 
-        string Key = key.Key;
+        CubeDetail cd2 = new CubeDetail();
+        
+
+        string Key;
         string result = string.Empty;
         string responseFromServer = string.Empty;
+        int progress_bar_Value = 0;
         List<CubeHistory> ListRawCubeHistory;
 
         public void GetData(int count, DateTime Fromdate, DateTime ToDate, string cursor)
         {
+            
             object obj = new object();
+            object obj2 = new object();
             DateTime _ToDate = ToDate;
             DateTime _Fromdate = Fromdate;
             ListRawCubeHistory = new List<CubeHistory>();
             TimeSpan subTime = _ToDate.Subtract(_Fromdate);
+            pBar.Maximum = subTime.Days+1;
+            Type type = typeof(CubeDetail);
+            FieldInfo[] f = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
             Parallel.For(0, subTime.Days, index =>
             {
+                
                 string strDate = (_ToDate.AddDays(-index)).ToString("yyyy-MM-dd");
 
                 string targetURL = $"https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results?count={count}&date={strDate}&cursor={cursor}";
@@ -92,8 +102,6 @@ namespace MaplestoryAPI
                         {
                             foreach (dynamic value2 in value)
                             {
-                                Type type = typeof(CubeDetail);
-                                FieldInfo[] f = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
                                 JProperty v2 = value2;
                                 foreach (FieldInfo f2 in f)
                                 {
@@ -121,23 +129,108 @@ namespace MaplestoryAPI
                             ch.next_cursor = next_cursor;
                             lock(obj){
                                 ListRawCubeHistory.Add(ch);
+                                
                             }
+                            
+
                         }
 
                     }
+
                 }
                 catch (Exception e)
                 {
+                    File.WriteAllText(Application.StartupPath+@"\error1.txt",e.ToString());
+                }
+
+            });
+
+            dataGridView1.Columns.Clear();
+            foreach (FieldInfo f2 in f)
+            {
+                Char[] splitchar = { '>', '<' };
+                if (f2.Name.Split(splitchar)[1].Contains("_potential_options"))
+                {
+                    for (int i = 1; i < 4; i++)
+                    {
+                        dataGridView1.Columns.Add(f2.Name.Split(splitchar)[1] + i, f2.Name.Split(splitchar)[1] + i);
+                    }
 
                 }
-            });
+                else
+                {
+                    dataGridView1.Columns.Add(f2.Name.Split(splitchar)[1], f2.Name.Split(splitchar)[1]);
+                }
+                
+            }
+            foreach (CubeHistory ch in ListRawCubeHistory)
+            {
+                object[] a = new object[dataGridView1.Columns.Count];
+
+                for (int i = 0; i < f.Length; i++)
+                {
+                    FieldInfo f2 = f[i];
+                    if (f2.Name.Contains("_potential_options"))
+                    {
+
+                        int idx = 0;
+                        if (f2.Name.Contains("after"))
+                            idx = 2;
+                        (string, string)[] tmp = ((string, string)[])f2.GetValue(ch.cube_histories);
+                        a[idx + i] = tmp[0];
+                        a[idx + i + 1] = tmp[1];
+                        a[idx + i + 2] = tmp[2];
+
+                    }
+                    else
+                    {
+                        a[i] = f2.GetValue(ch.cube_histories);
+                    }
+
+                }
+                dataGridView1.Rows.Add(a);
+            }
         }
 
      
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
+            if (Key ==null)
+            {
+                try
+                {
+                    GetKey();
+                }
+                catch
+                {
+                    OpenFileDialog ofd = new OpenFileDialog();
+                    ofd.ShowDialog();
+                    Key=File.ReadAllText(ofd.FileName);
+                }
+            }
+
             GetData(1000, dtpFrom.Value, dtpTo.Value, "");
+            
+        }
+
+        private void GetKey()
+        {
+            Key = File.ReadAllText(Application.StartupPath + @"\key.txt");
+        }
+
+        private void btnReadKey_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GetKey();
+            }
+            catch
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.ShowDialog();
+                Key = File.ReadAllText(ofd.FileName);
+            }
         }
     }
 }
